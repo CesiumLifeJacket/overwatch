e53b49f56abc968e3849c20b1f5c21f1639295b7
CesiumLifeJacket <CesiumLifeJacket@gmail.com>
2016-04-17 15:41:25 -0700

    reorganized project, refactored command line tool

---
+++ command-line/check-style.py
@@ -0,0 +1,23 @@
+'''
+read the .style.yml and apply appropriate stylecheckers to the appropriate files
+'''
+import os
+import json
+import yaml
+from glob import glob
+
+import pdb
+
+# load the configureation file
+config = yaml.load(open(".style.yml"))
+
+# generate the style report
+report = {} # filename: style violations pairs
+
+for stylechecker_name, patterns in config.items():
+	stylechecker = __import__("stylecheckers.{}".format(stylechecker_name), fromlist=['stylecheckers'])
+	files = sum((glob(pattern) for pattern in patterns), [])
+	report.update({f: stylechecker.check(f) for f in files})
+
+# Write out style report
+json.dump(report, open(".style_violations.json", 'w'))
