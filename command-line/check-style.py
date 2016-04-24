c3d4decd3b4a2414e9fee71f879d2101f73a4d34
CesiumLifeJacket <CesiumLifeJacket@gmail.com>
2016-04-24 16:37:51 -0700

    divs instead of spans

--- command-line/check-style.py
+++ command-line/check-style.py
@@ -14,8 +14,11 @@ from glob import glob
 
 import pdb
 
+# TODO: harden all this code; it will behave unpredictably when errors appear
+
 parser = argparse.ArgumentParser(description="check style for a project")
-parser.add_argument('web_directory', help="directory in which to write web report")
+parser.add_argument(
+    'web_directory', help="directory in which to write web report")
 
 args = parser.parse_args()
 
@@ -23,22 +26,25 @@ args = parser.parse_args()
 config = yaml.load(open(".style.yml"))
 
 # Perform the stylecheck and generate the report
-report = {} # filename: style violations pairs
+report = {}  # filename: style violations pairs
 
 for stylechecker_name, patterns in config.items():
-    stylechecker = __import__("stylecheckers.{}".format(stylechecker_name), fromlist=['stylecheckers'])
+    stylechecker = __import__("stylecheckers.{}".format(
+        stylechecker_name), fromlist=['stylecheckers'])
     files = sum((glob(pattern) for pattern in patterns), [])
     report.update({f: stylechecker.check(f) for f in files})
 
-## Write out style report
-#json.dump(report, open(".style_violations.json", 'w'),
+# Write out style report
+# json.dump(report, open(".style_violations.json", 'w'),
 #         sort_keys=True, indent=2, separators=(',', ': '))
 
 # Generate report webpages for each code file
+
+
 def annotate_code(code, violations):
     '''HTML escape the code and insert tags to mark style violations.
     '''
-    ## Calculate starting index of each line
+    # Calculate starting index of each line
 
     line_indices = [0]
 
@@ -46,41 +52,40 @@ def annotate_code(code, violations):
         line_index = line_indices[-1] + line_length
         line_indices.append(line_index)
 
-    ## Determine spans to insert and where to inesrt them
+    # Determine spans to insert and where to inesrt them
 
     # set of `(index, text)` pairs; `text` will be inserted at `index`
     insertions = set()
 
     for v in violations:
-        start_index = line_indices[v['row'] - 1] + v['col'] - 1
-        end_index = start_index + v.get('length', 1)
+        index = line_indices[v['row'] - 1] + v['col'] - 1
 
-        span_start = '<span class="violation" title="{}">'.format(
-                escape(v['message'])
-                )
-        span_end = '</span>'
+        insertion = '<div class="violation" type="{}" message="{}" len="{}"></div>'.format(
+            escape(v['type']),
+            escape(v['message']),
+            v.get('length', 1)
+        )
 
-        insertions.add((start_index, span_start))
-        insertions.add((end_index,   span_end  ))
+        insertions.add((index, insertion))
 
-    ## Build up the escaped code and inserted spans in list of segments
+    # Build up the escaped code and inserted spans in list of segments
 
     segments = []
     prev_index = 0
-    for index, span in sorted(insertions):
+    for index, insertion in sorted(insertions):
 
         # add escaped code
         segments.append(escape(code[prev_index:index]))
 
         # add tag
-        segments.append(span)
+        segments.append(insertion)
 
         prev_index = index
 
     # add the remaining text
     segments.append(escape(code[prev_index:]))
 
-    ## Join all the segments together and return annotated code
+    # Join all the segments together and return annotated code
     annotated_code = ''.join(segments)
 
     return annotated_code
@@ -90,12 +95,14 @@ if not os.path.exists(args.web_directory):
 
 source_path = os.path.dirname(os.path.realpath(__file__))
 template_path = os.path.join(source_path, 'web-template')
-env = Environment(loader=FileSystemLoader(template_path), trim_blocks=True, lstrip_blocks=True)
-env.filters['quote'] = escape 
+env = Environment(loader=FileSystemLoader(template_path),
+                  trim_blocks=True, lstrip_blocks=True)
+env.filters['quote'] = escape
 
 file_template = env.get_template("file.html")
 directory_template = env.get_template("directory.html")
-# TODO: copy over supporting CSS and JS
+
+# Copy over supporting CSS and JS
 css_path = os.path.join(args.web_directory, 'css')
 if not os.path.exists(css_path):
     shutil.copytree(os.path.join(template_path, 'css'), css_path)
@@ -112,40 +119,61 @@ for file, violations in report.items():
     html_filename = os.path.join(args.web_directory, "{}.html".format(file))
     open(html_filename, 'w').write(file_html)
 
+
+def path_filename(d):
+    if not hasattr(path_filename, 'filenames'):
+        setattr(path_filename, 'filenames', {})
+
+    if d in path_filename.filenames:
+        return path_filename.filenames[d]
+    else:
+        fname = d.replace('/', '__') + '.html'
+        path_filename.filenames[d] = fname
+        return fname
+
 directories = {}
 
-# add file to each directory
+# Add file to each directory
 for file, violations in report.items():
     directory, base = os.path.split(file)
     if directory not in directories:
         directories[directory] = []
     directories[directory].append({
         "name": base,
-        "path": file.replace('/', '__')+'.html',
+        "path": path_filename(file),
         "is_directory": False,
         "errors": len(violations)
-        })
+    })
 
-# TODO: Add subdirs to each directory. How?
+# Add subdirs to each directory.
+
 for directory in directories:
-    if not directory: continue
+    if not directory:
+        continue
 
     parent, base = os.path.split(directory)
 
     directories[parent].append({
         "name": base,
-        "path": directory.replace('/', '__')+'.html',
+        "path": path_filename(directory),
         "is_directory": True,
-        "files": len(directories[base]) # TOOD: harden this
-        })
+        "files": len(directories[base])
+    })
 
-# create HTML for each directory
+    # Add parent dir link to child directories
+    #directories[base].append({
+    #    "name": "..",
+    #    "path": path_filename(parent)
+    #})
+
+# Create HTML for each directory
 for directory, contents in directories.items():
     dirname = directory or 'Index'
-    directory_html = directory_template.render(directory=dirname,contents=contents)
+    directory_html = directory_template.render(
+        directory=dirname, contents=contents)
     dirname = dirname.replace('/', '__')
     html_filename = os.path.join(args.web_directory, "{}.html".format(dirname))
     open(html_filename, 'w').write(directory_html)
 
-#json.dump(directories, sys.stdout,
+# json.dump(directories, sys.stdout,
 #         sort_keys=True, indent=2, separators=(',', ': '))
