fa9c104256176c23f999cf7f82d11977738e37c9
CesiumLifeJacket <CesiumLifeJacket@gmail.com>
2016-04-26 14:43:36 -0700

    cleaned up unused files and directories

--- command-line/directory.py
+++
@@ -1,5 +0,0 @@
-from cgi import escape
-import jinja2
-
-template = jinja2.Template(open('web-template/directory.html').read())
-
--- web-old/webtest.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import cgitb
-cgitb.enable()
-print("Content-Type: text/html")
-print
-
-print("<h1> It Worked! </h1>")
