4b1c07b38c030d7ec3e4cf0e514ca90a6ec8af5d
CesiumLifeJacket <CesiumLifeJacket@gmail.com>
2016-05-12 15:08:11 -0700

    added py3 support and robustness

--- command-line/check-style.py
+++ command-line/check-style.py
@@ -1,51 +1,80 @@
 '''
-Read the .style.yml and apply appropriate stylecheckers to the appropriate files
-write out the results to .style_violations.json
+Author: Jeremy Vonderfecht
+
+Command-line utility which checks code style against a standard and produces
+html-based report in specified directory. Read the .style.yml and apply
+appropriate stylecheckers to the appropriate files write out the results to
+web_directory as specified by command line argument
 '''
 import os
 import sys
-from cgi import escape
 import json
 import yaml
 import shutil
-from jinja2 import Environment, FileSystemLoader
 import argparse
 from glob import glob
+from cgi import escape
+from jinja2 import Environment, FileSystemLoader
 
 import pdb
 
-# TODO: harden all this code; it will behave unpredictably when errors appear
+# TODO: be consistent with var name conventions, etc
 
-parser = argparse.ArgumentParser(description="check style for a project")
+# TODO: more helpful help messages
+parser = argparse.ArgumentParser(
+    description="check code style of a project based upon settings in .style.yml")
 parser.add_argument(
     'web_directory', help="directory in which to write web report")
 
 args = parser.parse_args()
 
 # load the configureation file
+if not os.path.exists('.style.yml'):
+    print("No .style.yml found.")
+    exit(1)
+
 config = yaml.load(open(".style.yml"))
+
+# TODO: check format of config against Rx schema
 
 # Perform the stylecheck and generate the report
 report = {}  # filename: style violations pairs
 
 for stylechecker_name, patterns in config.items():
-    stylechecker = __import__("stylecheckers.{}".format(
-        stylechecker_name), fromlist=['stylecheckers'])
-    files = sum((glob(pattern) for pattern in patterns), [])
-    report.update({f: stylechecker.check(f) for f in files})
+    # TODO: report error when stylechecker isn't found
+    try:
+        stylechecker = __import__("stylecheckers.{}".format(
+            stylechecker_name), fromlist=['stylecheckers'])
+    except:
+        print("Could not find styechecker '{}'.".format(stylechecker_name))
+        exit(1)
 
-# Write out style report
-# json.dump(report, open(".style_violations.json", 'w'),
-#         sort_keys=True, indent=2, separators=(',', ': '))
+    files = []
+    for pattern in patterns:
+        filesMatchingPattern = glob(pattern)
+
+        if not filesMatchingPattern:
+            print("Warning: no files match pattern {}".format(pattern))
+
+        files += filesMatchingPattern
+
+    report_files = {}
+    for f in files:
+        try:
+            report_files[f] = stylechecker.check(f)
+        except:
+            print("Error checking file {}".format(f))
+
+    report.update(report_files)
 
 # Generate report webpages for each code file
 
 
 def annotate_code(code, violations):
+    # TODO: more thorough docstring here
     '''HTML escape the code and insert tags to mark style violations.
     '''
     # Calculate starting index of each line
-
     line_indices = [0]
 
     for line_length in map(len, code.split('\n')):
@@ -60,11 +89,12 @@ def annotate_code(code, violations):
     for v in violations:
         index = line_indices[v['row'] - 1] + v['col'] - 1
 
-        insertion = '<div class="violation" type="{}" style="width:{}ch;"><div class="error">{}</div></div>'.format(
-            escape(v['type']),
-            v.get('length', 1),
-            escape(v['message'])
-        )
+        insertion = '<div class="violation" type="{}" style="width:{}ch;">' \
+                    + '<div class="error">{}</div></div>'.format(
+                        escape(v['type']),
+                        v.get('length', 1),
+                        escape(v['message'])
+                    )
 
         insertions.add((index, insertion))
 
@@ -90,7 +120,10 @@ def annotate_code(code, violations):
 
     return annotated_code
 
+
 def path_filename(d):
+    '''Returns a unique 
+    '''
     if not hasattr(path_filename, 'filenames'):
         setattr(path_filename, 'filenames', {})
         path_filename.filenames[''] = 'Index.html'
@@ -99,10 +132,22 @@ def path_filename(d):
         return path_filename.filenames[d]
     else:
         fname = d.replace('/', '__') + '.html'
-        path_filename.filenames[d] = fname
+        if fname not in path_filename.filenames.values():
+            path_filename.filenames[d] = fname
+        else:
+            i = 1
+            old_fname = fname
+            fname = old_fname+'({})'.format(i)
+            while fname in path_filename.filenames.values():
+                i += 1
+                fname = old_fname+'({})'.format(i)
+            path_filename.filenames[d] = fname
+
         return fname
 
 def get_parents(path):
+    '''TODO: docstring
+    '''
     if not path:
         return [("/", path_filename(''))]
 
@@ -111,15 +156,22 @@ def get_parents(path):
 
     return get_parents(parent) + [(base, fname)]
 
+# TODO: refactor, modularize this
+
+# TODO: if it exists already, 
+# completely clear this directory before repopulating
+
 if not os.path.exists(args.web_directory):
     os.makedirs(args.web_directory)
 
+# Set up Jinja2 template envronment
 source_path = os.path.dirname(os.path.realpath(__file__))
 template_path = os.path.join(source_path, 'web-template')
 env = Environment(loader=FileSystemLoader(template_path),
                   trim_blocks=True, lstrip_blocks=True)
 env.filters['quote'] = escape
 
+# get jinja templates
 file_template = env.get_template("file.html")
 directory_template = env.get_template("directory.html")
 
@@ -147,7 +199,7 @@ for file, violations in report.items():
 
 directories = {}
 
-# Add file to each directory
+## Add file to each directory
 for file, violations in report.items():
     directory, base = os.path.split(file)
     if directory not in directories:
@@ -160,7 +212,6 @@ for file, violations in report.items():
     })
 
 # Add subdirs to each directory.
-#pdb.set_trace()
 for directory in directories:
     if not directory:
         continue
@@ -174,15 +225,8 @@ for directory in directories:
         "files": len(directories[directory])
     })
 
-    # Add parent dir link to child directories
-    #directories[base].append({
-    #    "name": "..",
-    #    "path": path_filename(parent)
-    #})
-
 # Create HTML for each directory
 for directory, contents in directories.items():
-    # TODO: split up the whole directory path and create links at each level
     dir_path = get_parents(directory)
     directory_html = directory_template.render(
         dir_path=dir_path, contents=contents)
@@ -191,5 +235,3 @@ for directory, contents in directories.items():
     html_filename = os.path.join(args.web_directory, dirname)
     open(html_filename, 'w').write(directory_html)
 
-# json.dump(directories, sys.stdout,
-#         sort_keys=True, indent=2, separators=(',', ': '))
